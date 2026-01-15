6fab335f9e919e2e42556805cbc041a4de8078d2
christianwl <chris2w0social@gmail.com>
2026-01-15 05:29:19 -0300

    feat: adicionar executarDesafios via JS, adicionar reset e base css e atualizar infos no index.html e readme

--- Desafios/LogicaJS/appLogicaJS.js
+++ Desafios/LogicaJS/appLogicaJS.js
@@ -1,5 +1,10 @@
 import { executarPrimeiroDesafio } from "./Dia1.js";
 import { executarSegundoDesafio } from "./Dia2.js";
 
-executarPrimeiroDesafio();
-executarSegundoDesafio();
+function executarDesafios(){
+    executarPrimeiroDesafio();
+    executarSegundoDesafio();
+}
+
+const botao = document.getElementById('btn-iniciar');
+botao.addEventListener('click', executarDesafios);
