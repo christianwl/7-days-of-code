ac16af17154b3942474b41296139cacdc2d4cde4
christianwl <chris2w0social@gmail.com>
2026-03-13 23:19:51 -0300

    refactor: rename codes to english

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,10 @@
 import { showDay1 } from "./modules/day-1.js";
 import { showDay2 } from "./modules/day-2.js";
 
-function executarDesafios() {
+function startChallenges() {
   showDay1();
   showDay2();
 }
 
-const botao = document.getElementById("btn-iniciar");
-botao.addEventListener("click", executarDesafios);
+const botao = document.getElementById("btn-start");
+botao.addEventListener("click", startChallenges);
